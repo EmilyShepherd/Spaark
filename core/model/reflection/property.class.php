4600fb7c5a41a6ff406306ae107b50bab8e0e835
Emily Shepherd <emily@emilyshepherd.me>
2014-12-24 12:16:10 +0000

    Made Property Reflection better and generalised the Reflector

--- core/model/reflection/property.class.php
+++ core/model/reflection/property.class.php
@@ -4,115 +4,159 @@
  *
  */
 
+/**
+ * Reflects upon properies within a model, and parses their doc comments
+ */
 class Property extends Reflector
 {
     /**
-     * @readonly
+     * This class will be instanciated to $this->object
+     */
+    const WRAPPER_NAME = 'ReflectionProperty';
+
+    /**
+     * List of accepted parameters in a doc comment in the form:
+     *   name => function_to_call
+     */
+    protected $acceptedParams = array
+    (
+        'localkey'   => 'mixed',
+        'foreignkey' => 'mixed',
+        'linktable'  => 'mixed',
+        'readonly'   => 'bool',
+        'save'       => 'bool',
+        'id'         => 'setKey',
+        'type'       => 'setType'
+    );
+
+    /**
+     * Should this value be saved to a source?
      */
     protected $save;
 
     /**
-     * @readonly
+     * Is this property read only?
      */
     protected $readonly;
 
     /**
-     * @readonly
+     * What kind of property is this? Primary / Unique etc
      */
     protected $key;
 
     /**
-     * @readonly
+     * 
      */
     protected $link;
 
+    /**
+     * This property's type
+     */
     protected $type;
+
+    /**
+     * Will this property be an array of multiple items, or just one
+     */
     protected $many = false;
+
+    /**
+     * 
+     */
     protected $from;
-    protected $localKey;
-    protected $foreignKey;
-    protected $linkTable;
+
+    /**
+     * If the property is an object, it will be loaded. These specify
+     * the links in the data structures which link these together.
+     *
+     * By default, the localkey is the property name, with '_id'
+     * appended and foreignkey is just 'id'
+     */
+    protected $localkey, $foreignkey;
+
+    /**
+     * When links represent a many to many relationship, an intermediary
+     * table can be used
+     */
+    protected $linktable;
+
+    /**
+     * 
+     */
     protected $direction;
 
-    const WRAPPER_NAME = 'ReflectionProperty';
-
+    /**
+     * Parses the doctype and infers the foreign and local keys if they
+     * were not set explicitly
+     *
+     * @see Reflector::parse()
+     */
     public function parse()
     {
-        preg_match_all
-        (
-              '/^[ \t]*\*[ \t]*@([a-zA-Z]*)[ \t]*'
-            . '('
-            .     '((array)\(([a-zA-Z0-9]+)(\.([a-zA-Z0-9]+))?\))' . '|'
-            .     '(' .     '([a-zA-Z0-9]+)(\.([a-zA-Z0-9]+))?)'
-            . ')'
-            . '\s*$/m',
-            $this->object->getDocComment(),
-            $matches
-        );
+        parent::parse();
 
-        var_dump($matches);
+        $this->object->setAccessible(true);
 
-        foreach ($matches[0] as $i => $value)
+        if (!$this->foreignkey && !$this->localkey)
         {
-            $name = $matches[1][$i];
-            $data = trim($matches[2][$i]);
-
-            switch ($name)
-            {
-                case 'localKey':
-                case 'foreignKey':
-                case 'linkTable':
-                case 'key':
-                    $this->$name = $data;
-                    break;
-
-                case 'readonly':
-                    $this->readonly = true;
-                    break;
-
-                case 'save':
-                    if (!$data || strtolower($data) == 'true')
-                    {
-                        $this->save = true;
-                    }
-                    elseif (strtolower($data) == 'false')
-                    {
-                        $this->save = false;
-                    }
-                    else
-                    {
-                        //
-                    }
-                    break;
-
-                case 'id':
-                    $this->key = 'primary';
-                    break;
-
-                case 'type':
-                    if ($matches[4][$i] == 'array')
-                    {
-                        $this->many = true;
-                    }
-
-                    $this->type = $matches[5][$i];
-                    break; 
-            }
+            $this->localkey   = $this->object->getName() . '_id';
+            $this->foreignkey = 'id';
         }
-
-        if (!$this->foreignKey && !$this->localKey)
+        elseif (!$this->localkey)
         {
-            $this->localKey   = $this->object->getName() . '_{id}';
-            $this->foreignKey = '{id}';
+            $parts          = explode('_', $this->foreignkey);
+            $this->localkey = $parts[1];
         }
-        elseif (!$this->localKey)
+        elseif (!$this->foreignkey)
         {
-            $parts          = explode('_', $this->foreignKey);
-            $this->localKey = $parts[1];
+            throw new \Exception('Unable to determin foreignkey');
         }
-        elseif (!$this->foreignKey)
+    }
+
+    /**
+     * Sets the key to be primary
+     *
+     * @param string $name Ignored
+     * @param mixed $value Ignored
+     */
+    protected function setKey($name, $value)
+    {
+        $this->key = 'primary';
+    }
+
+    /**
+     * Sets the type
+     *
+     * This should be a model name, either on it's own or in brackets
+     * with "array". Eg:
+     *   + ModelName
+     *   + array(ModelName)
+     *
+     * @param string $name Ignored
+     * @param string $value The value to set
+     */
+    protected function setType($name, $value)
+    {
+        if (!preg_match('/^array\((.*?)\)$/', $value, $match))
         {
-            throw new \Exception('Unable to determin foreignKey');
+            $this->type = $value;
         }
+        else
+        {
+            $this->many = true;
+            $this->type = $match[1];
+        }
+    }
+
+    /**
+     * @getter save
+     */
+    public function isProperty()
+    {
+        return (boolean)$this->type;
+    }
+
+    public function getValue($obj)
+    {
+        return $this->object->getValue($obj);
     }
 }
--- core/model/reflection/reflector.class.php
+++ core/model/reflection/reflector.class.php
@@ -7,15 +7,37 @@ use \Spaark\Core\Model\Base;
 
 class Reflector extends \Spaark\Core\Model\Base\Wrapper
 {
+// {{{ static
+
+    /**
+     * This class will be instanciated to $this->object
+     *
+     * It should be one of PHP's inbuilt reflection classes
+     */
     const WRAPPER_NAME = 'ReflectionClass';
 
-    protected function __fromRef($cb)
-    {
-        $class        = static::WRAPPER_NAME;
-        $this->object = new $class($cb[0], $cb[1]);
-    }
+    /**
+     * List of accepted parameters in a doc comment in the form:
+     *   name => function_to_call
+     */
+    protected $acceptedParams = array( );
 
-    protected function _fromRef($ref)
+    /**
+     * Checks that a reference is formatted correctly, then calls
+     * fromRef() again
+     *
+     * This is used so you can use multiple forms of a reference whilst
+     * still getting the same cached object. For example, the following
+     * will all return the same object:
+     *   "\My\Class::method"
+     *   array("\My\Class", "method")
+     *   array(instanceof("\My\Class"), "method")
+     *   array(instanceofreflectorfor("\My\Class"), "method")
+     *
+     * @param mixed $ref A reference to get a reflector for
+     * @return Reflector A reflector for the given reference
+     */
+    protected static function _fromRef($ref)
     {
         if (static::isRefCorrect($ref)) return;
 
@@ -25,8 +47,8 @@ class Reflector extends \Spaark\Core\Model\Base\Wrapper
 
             return static::fromRef(array
             (
-                                 static::getName($ref[0]),
-                isset($ref[1]) ? static::getName($ref[1]) : ''
+                                 static::findName($ref[0]),
+                isset($ref[1]) ? static::findName($ref[1]) : ''
             ));
         }
         elseif (is_string($ref))
@@ -38,7 +60,7 @@ class Reflector extends \Spaark\Core\Model\Base\Wrapper
         }
         elseif (is_object($ref))
         {
-            return static::fromRef(array(static::getName($ref)));
+            return static::fromRef(array(static::findName($ref)));
         }
         else
         {
@@ -51,15 +73,30 @@ class Reflector extends \Spaark\Core\Model\Base\Wrapper
         }
     }
 
-    private function getName($ref)
+    /**
+     * Finds the name of a class, from a reference
+     *
+     * This function uses the following logic:
+     *   + If the given reference is a string, this is assumed to be the
+     *     name of the class.
+     *   + If the given reference is an instance of a reflector, the
+     *     name of the class it is reflecting on is the name of the
+     *     class
+     *   + If the given reference is an instance of another class, its
+     *     class name is used
+     *
+     * @param mixed $ref A string / object for a class
+     * @return string The name of the class
+     */
+    private static function findName($ref)
     {
         if (is_string($ref))
         {
             return ltrim($ref, '\\');
         }
-        elseif (is_a($ref, '\Reflector') || is_a($ref, get_class()))
+        elseif (is_a($ref, '\Reflector') || is_a($ref, __CLASS__))
         {
-            return ltrim($cb->getName(), '\\');
+            return ltrim($ref->getName(), '\\');
         }
         elseif (is_object($ref))
         {
@@ -67,7 +104,15 @@ class Reflector extends \Spaark\Core\Model\Base\Wrapper
         }
     }
 
-    private function isRefCorrect($ref)
+    /**
+     * Returns true if a reference is in the standard format
+     *
+     * The standard format is defined as an array containing two strings
+     *
+     * @param mixed $ref The reference to check
+     * @return bool True if the given reference is in the correct format
+     */
+    private static function isRefCorrect($ref)
     {
         if
         (
@@ -80,7 +125,12 @@ class Reflector extends \Spaark\Core\Model\Base\Wrapper
         }
     }
 
-    private function checkCount($ref, $original = NULL)
+    /**
+     * Ensures that a reference array has either 1 or 2 entries.
+     *
+     * @throws CannotCreateModelException if there aren't 1 or 2
+     */
+    private static function checkCount($ref, $original = NULL)
     {
         if (count($ref) != 1 && count($ref) != 2)
         {
@@ -92,4 +142,99 @@ class Reflector extends \Spaark\Core\Model\Base\Wrapper
             );
         }
     }
+
+    // }}}
+
+        ////////////////////////////////////////////////////////
+
+// {{{ instance
+
+    /**
+     * Creates an instance of the Reflection Class as specified by
+     * WRAPPER_NAME and starts parsing the doctype
+     *
+     * @param array $cb A valid reference to reflect on
+     */
+    protected function __fromRef($cb)
+    {
+        $class        = static::WRAPPER_NAME;
+        $this->object = new $class($cb[0], $cb[1]);
+
+        $this->parse();
+    }
+
+    /**
+     * Parses the doctype and calls subclass methods to handle each
+     * param
+     */
+    public function parse()
+    {
+        preg_match_all
+        (
+              '/^'
+            .     '[ \t]*\*[ \t]*'
+            .     '@([a-zA-Z0-9]+)'
+            .     '(.*)'
+            . '$/m',
+            $this->object->getDocComment(),
+            $matches
+        );
+
+        foreach ($matches[0] as $key => $value)
+        {
+            $name  = strtolower($matches[1][$key]);
+            $value = trim($matches[2][$key]);
+
+            if (isset($this->acceptedParams[$name]))
+            {
+                call_user_func
+                (
+                    array($this, $this->acceptedParams[$name]),
+                    $name, $value
+                );
+            }
+        }
+    }
+
+    /**
+     * Sets the given property with the given value
+     *
+     * $this->$name = $value
+     *
+     * @param string $name The property to set
+     * @param string $value The value to set
+     */
+    protected function mixed($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+    /**
+     * Sets the given property with the given boolean value
+     *
+     * The value is parsed such that the string "false" and "0" also
+     * count as false
+     *
+     * @param string $name The property to set
+     * @param string $value The value to set
+     */
+    protected function bool($name, $value)
+    {
+        $value = strtolower($value);
+
+        if ($value == 'true')
+        {
+            $this->$name = TRUE;
+        }
+        elseif ($value == 'false')
+        {
+            $this->$name = FALSE;
+        }
+        else
+        {
+            $this->$name = (boolean)$value;
+        }
+    }
+
+    // }}}
 }
