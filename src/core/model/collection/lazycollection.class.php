cc81a0e2d079d8dcfe921811e23e189f977f220f
Emily Shepherd <emily@emilyshepherd.me>
2016-07-17 20:07:15 +0100

    Implement LazyCollection to use offsetGet

--- src/core/model/collection/lazycollection.class.php
+++ src/core/model/collection/lazycollection.class.php
@@ -11,15 +11,42 @@
  *
  * @author Emily Shepherd
  */
-class LazyCollection extends Collection
+abstract class LazyCollection extends Collection
 {
-    private $built = array( );
+    /**
+     * Attempts to build the value at the given position
+     *
+     * This method is normally called by offsetGet when a value is
+     * asked for which does not appear to exist. This method must
+     * generate and return the specific value that was asked for,
+     * howver it is acceptable for it to generate more items and add
+     * these to $data if this is efficient.
+     *
+     * @param scalar $position The position to build
+     * @return mixed The value of that position
+     */
+    abstract protected function get($position);
 
-    private $collection;
-
-    public function next()
+    /**
+     * Returns the data at the given offset
+     *
+     * If the offset does not exist, and the lazy collection does not
+     * appear to have finished building itself, this method will call
+     * the child class to ask it to attempt to build the value.
+     *
+     * @param scalar $offset The offset to try to get
+     * @return mixed The value at the given offset
+     * @see get($position)
+     */
+    public function offsetGet($position)
     {
-        $this->pointer++;
-
+        if (isset($this->data[$position]))
+        {
+            return $this->data[$position];
+        }
+        elseif (count($this->data) < $this->size())
+        {
+            return $this->data[$position] = $this->get($position);
+        }
     }
 }
